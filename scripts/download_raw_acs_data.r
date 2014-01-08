abf399893514f1b0cb149005638e444fa23db160
nsmader <nsmader@gmail.com>
2014-01-08 00:11:43 -0600

    Generated pseudo-code for processing tables

--- scripts/download_raw_acs_data.r
+++ scripts/download_raw_acs_data.r
@@ -9,21 +9,23 @@
 
 ### Set Up Workspace
 
+  rm(list=ls())
   "%&%" <- function(...){ paste(..., sep="")}
   library(acs) # This package isn't (yet) used directly to download ACS data, since it generates pulls using the Census API, and 
     #   only a subset of Census data sets are available through the API. However, it has some useful helper functions to
     #   find codes for tables and geographies
   
-  myLocalUser <- "nmader.CHAPINHALL"
-  rootDir <- "C:/Documents and Settings/" %&% myLocalUser %&% "/My Documents/GitHub/acs-prep-cook-serve/"
-  dlDir <- "C:/Documents and Settings/" %&% myLocalUser %&% "/My Documents/GitHub/acs-prep-cook-serve/data/raw-downloads/"
+  #myLocalUser <- "nmader.CHAPINHALL"
+  myLocalUser <- "nmader"
+  rootDir <- "C:/Users/nmader/Documents/GitHub/acs-shop-cook-serve/"
+  dlDir <- rootDir %&% "data/raw-downloads/"
   setwd(dlDir)
 
 ### Define Pulls
 
   # NSM: we will want to convert this to a function so that users can call on this multiple times for different types of pulls, either with separate years
 
-  pullYear <- c("2012")
+  pullYear <- "2012"
   pullSpan  <- 1
   pullState <- "Illinois"
   pullCtny  <- c("Cook County", "Will County", "Lake County", "Kane County", "McHenry County", "DuPage County")  ## To avoid potentially non-unique pulls, "County" should be specified here
@@ -42,56 +44,47 @@
   myPathFileName <- dlDir %&% "Illinois_All_Geographies.zip"
   remoteDataName <- paste0("http://www2.census.gov/acs", pullYear, "_", pullSpan, "yr/summaryfile/", pullYear, "_ACSSF_By_State_All_Tables/", pullState, "_All_Geographies.zip")
 
-  tableLookup <- read.csv(url(paste0("http://www2.census.gov/acs", myYear, "_", pullSpan, "yr/summaryfile/Sequence_Number_and_Table_Number_Lookup.txt"), header = TRUE)
+  tableLookup <- read.csv(url(paste0("http://www2.census.gov/acs", pullYear, "_", pullSpan, "yr/summaryfile/Sequence_Number_and_Table_Number_Lookup.txt")), header = TRUE)
   download.file(remoteDataName, myPathFileName)
   unzip(zipfile = myPathFileName) # NSM: am having problems explicitly feeding an argument to "exdir" for this function.
-                                  # For now, it's using the current working directory as the default.
+                                  # For now, it's using the current working directory as the default
+  
+### Select Tables and Merge Together
+
+  # Identify the sequence number corresponding to each table that has been specified
+    # Requires use of metadata? From the tableLookup file?
+    # Colnames for the table lookup file are: "File.ID", "Table.ID", "Sequence.Number", "Line.Number", "Start.Position",
+      # "Total.Cells.in.Table", "Total.Cells.in.Sequence", "Table.Title", "Subject.Area"  
+
+  # Pull those sequence files
+    # Should allow choice of E and M--or both--sets of tables
+
+  # Generate headers for the files based on standard fields, and additional fields based on meta-data
+
+    # Based on the read files from SAS, the standard set of fields includes--
+#       FILEID  ='File Identification'
+#       FILETYPE='File Type'
+#       STUSAB  ='State/U.S.-Abbreviation (USPS)'
+#       CHARITER='Character Iteration'
+#       SEQUENCE='Sequence Number'
+#       LOGRECNO='Logical Record Number' # ... looks like the geo file has the right reference for this, which has values 1:270 in field LOGRECNO (same as the data file, except for leading zeroes)
+#       ... In the file for sequence number 31 there are 36 values. The metafile shows 30 non-NA values in the "Line.Number" field, and there are 6 standard variables.
+#       .... The problem is that metadatafile has information on *many* tables! I'd expected this to be unique! Perhaps it didn't read in properly?
+#       .... Nevermind! There really is data on multiple tables in each sequence table. What the hell?
+#       ... the "Start.Position" is the cue for the starting column (rather than character) of the table in question.
+#       ... There are large gaps in the tables which, in the data that I've downloaded, correspond to the lack of data on Puerto Rico
+
+  # Horizontally merge the files
+
+  # Use the geo file to select among the rows in the sequence tables
+
 
   geoLabels <- read.csv(paste0(rootDir, "data/prepped-data/geofile-fields.csv"), header=T) # Note--this file was created by hand from the labels in the SAS version of the data prep file. See ".../scripts/Summary file assembly script from Census.sas"
   geoFile <- read.csv(paste0(dlDir, "g", pullYear, pullSpan, tolower(pullSt), ".csv"), header=F)
   colnames(geoFile) <- geoLabels$geoField
-  
-  geoLabels <- c()
-  names(geoLabels) <- c("FILEID", "STUSAB", "SUMLEVEL", "COMPONENT", "LOGRECNO", "US", "REGION", "DIVISION", "STATECE", "STATE", "COUNTY", "COUSUB", "PLACE", "TRACT", "BLKGRP", "CONCIT", "CSA", "METDIV", "UA", "UACP", "VTD", "ZCTA3", "SUBMCD", "SDELM", "SDSEC", "SDUNI", "UR", "PCI", "TAZ", "UGA", "GEOID", "NAME", "AIANHH", "AIANHHFP", "AIHHTLI", "AITSCE", "AITS", "ANRC", "CBSA", "MACC", "MEMI", "NECTA", "CNECTA", "NECTADIV", "CDCURR", "SLDU", "SLDL", "ZCTA5", "PUMA5", "PUMA1")
-    
-  
-    
-      FILEID  ='File Identification'           STUSAB   ='State Postal Abbreviation' 		SUMLEVEL='Summary Level'            	COMPONENT='geographic Component'
-		LOGRECNO='Logical Record Number'	    US       ='US'
-		REGION  ='Region'						DIVISION ='Division'
-		STATECE ='State (Census Code)'			STATE    ='State (FIPS Code)'
-		COUNTY  ='County'						COUSUB   ='County Subdivision (FIPS)'
-		PLACE   ='Place (FIPS Code)'			TRACT    ='Census Tract'
-		BLKGRP  ='Block Group'					CONCIT   ='Consolidated City'
-		CSA     ='Combined Statistical Area'	METDIV  ='Metropolitan Division'
-		UA      ='Urban Area'                   UACP    ='Urban Area Central Place'
-		VTD     ='Voting District'				ZCTA3  ='ZIP Code Tabulation Area (3-digit)'
-		SUBMCD  ='Subbarrio (FIPS)'				SDELM  ='School District (Elementary)'
-		SDSEC   ='School District (Secondary)'	SDUNI  ='School District (Unified)'
-		UR      ='Urban/Rural'					PCI    ='Principal City Indicator'
-		TAZ     ='Traffic Analysis Zone'		UGA    ='Urban Growth Area'
-		GEOID   ='geographic Identifier'		NAME   ='Area Name' 					    
-		AIANHH  ='American Indian Area/Alaska Native Area/Hawaiian Home Land (Census)'
-		AIANHHFP='American Indian Area/Alaska Native Area/Hawaiian Home Land (FIPS)'
-		AIHHTLI ='American Indian Trust Land/Hawaiian Home Land Indicator'
-		AITSCE  ='American Indian Tribal Subdivision (Census)'
-		AITS    ='American Indian Tribal Subdivision (FIPS)'
-		ANRC    ='Alaska Native Regional Corporation (FIPS)'
-		CBSA    ='Metropolitan and Micropolitan Statistical Area'
-		MACC    ='Metropolitan Area Central City'	
-		MEMI    ='Metropolitan/Micropolitan Indicator Flag'
-		NECTA   ='New England City and Town Combined Statistical Area'
-		CNECTA  ='New England City and Town Area'
-		NECTADIV='New England City and Town Area Division'
-		CDCURR  ='Current Congressional District'
-		SLDU    ='State Legislative District Upper'	
-		SLDL    ='State Legislative District Lower'
-		ZCTA5   ='ZIP Code Tabulation Area (5-digit)'
-		PUMA5   ='Public Use Microdata Area - 5% File'
-		PUMA1   ='Public Use Microdata Area - 1% File'	
-    
-    
-### Select Tables and Merge Together
+
+  # ... what is the particular syntax of sequence tables?
+  # A good way to add leading 0s is sprintf, e.g.: sprintf("%010d", 104)
 
   a <- read.csv(paste0(dlDir, "e20121il0001000.txt"), header=F)
   g <- read.csv(paste0(dlDir, "g20121il.csv"), header=F)
