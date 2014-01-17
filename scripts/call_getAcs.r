78aaf578f0aaefbdae3ceeb5d5a1fa42ca322eb7
nsmader <nsmader@gmail.com>
2014-01-17 17:19:26 -0600

    cleaned dl function

--- scripts/call_getAcs.r
+++ scripts/call_getAcs.r
@@ -5,13 +5,16 @@
 
   #myLocalUser <- "nmader.CHAPINHALL"
   myLocalUser <- "nmader"
-  rootDir <- "C:/Users/nmader/Documents/GitHub/acs-shop-cook-serve/"
-  dlDir <- rootDir %&% "data/raw-downloads/"
-  saveDir <- rootDir %&% "data/prepped-data/"
-  setwd(dlDir)
-  source("download_acs_fn.r")
+  dirRoot <- "C:/Users/nmader/Documents/GitHub/acs-shop-cook-serve/"
+  dirScripts <- dirRoot %&% "scripts/"
+  dirDl <- dirRoot %&% "data/raw-downloads/"
+  dirSave <- dirRoot %&% "data/prepped-data/"
+  setwd(dirDl)
+  source(dirScripts %&% "download_acs_fn.r")
 
+#---------------
 ### Set up pulls
+#---------------
   downloadData <- FALSE
   myYear <- "2012"
   mySpan  <- 1
@@ -20,7 +23,12 @@
   myCounties  <- c("Cook County", "Will County", "Lake County", "Kane County", "McHenry County", "DuPage County")
   myTables <- unlist(strsplit("B01001 B01001A B01001B B01001C B01001D B01001E B01001F B01001G B01001H B01001I B08006 B08008 B08011 B08012 B08013 B15001 B15002 B17001 B12001 B12002 B12006 B17003 B17004 B17005 B19215 B19216 B14004 B14005 B05003 B23001 B23018 B23022 B24012 B24022 B24042 B24080 B24082 B24090 C24010 C24020 C24040 B11001 B11003 B11004 B13002 B13012 B13014 B13016 B17022 B23007 B23008 B25115", split= " "))
 
+#-----------------
+### Call for pulls 
+#-----------------
+  myData <- getAcs(pullYear = 2012, pullSpan = 1, pullState = "Illinois", pullSt = "IL", pullCounties = myCounties, pullTables = myTables, dirDl = dirDl, downloadData = TRUE)
+  writecsv(myData, paste0(dirSave, "ACS_", pullYear, "_", pullSpan, "Year_", pullSt, "_PreppedVars.csv"))
 
-  myData <- getAcs(pullYear = 2012, pullSpan = 1, pullState = "Illinois", pullSt = "IL", pullCounties = myCounties, pullTables = myTables, dlDir = dlDir, downloadData = TRUE)
-  writecsv(myData, paste0(saveDir, "ACS_", pullYear, "_", pullSpan, "Year_", pullSt, "_PreppedVars.csv"))
 
+  
+  
--- scripts/download_acs_fn.r
+++ scripts/download_acs_fn.r
@@ -5,10 +5,9 @@
 # Authors: Nick Mader (nmader@chapinhall.org), ...
 #
 #########################################################################
-
-  library(acs) # This package isn't (yet) used directly to download ACS data, since it generates pulls using the Census API, and 
-    #   only a subset of Census data sets are available through the API. However, it has some useful helper functions to
-    #   find codes for tables and geographies
+library(acs) # This package isn't (yet) used directly to download ACS data, since it generates pulls using the Census API, and 
+  #   only a subset of Census data sets are available through the API. However, it has some useful helper functions to
+  #   find codes for tables and geographies
 
 getAcs <- function(pullYear, pullSpan, pullState, pullSt, pullCounties, pullTables, dlDir, downloadData) {
 
@@ -19,11 +18,18 @@ getAcs <- function(pullYear, pullSpan, pullState, pullSt, pullCounties, pullTabl
 ### Download and Extract Data
 #----------------------------
   
-  myPathFileName <- paste0(dlDir, "ACS_", pullYear, "_" pullSpan, "Year_", pullSt, ".zip")
-  remoteDataName <- paste0("http://www2.census.gov/acs", pullYear, "_", pullSpan, "yr/summaryfile/", pullYear, "_ACSSF_By_State_All_Tables/", pullState, "_All_Geographies.zip")
-
+  # Get metadata
   Meta <- read.csv(url(paste0("http://www2.census.gov/acs", pullYear, "_", pullSpan, "yr/summaryfile/Sequence_Number_and_Table_Number_Lookup.txt")), header = TRUE)
-  if (downloadData == TRUE) {
+  
+  # Get geodata
+  geoLabels <- read.csv(paste0(rootDir, "data/prepped-data/geofile-fields.csv"), header=T) # created by hand from documentation
+  geoFile <- read.csv(paste0(dlDir, "g", pullYear, pullSpan, tolower(pullSt), ".csv"), header=F)
+  colnames(geoFile) <- geoLabels$geoField
+  
+  # Get data
+  myPathFileName <- paste0(dlDir, "ACS_", pullYear, "_", pullSpan, "Year_", pullSt, ".zip")
+  remoteDataName <- paste0("http://www2.census.gov/acs", pullYear, "_", pullSpan, "yr/summaryfile/", pullYear, "_ACSSF_By_State_All_Tables/", pullState, "_All_Geographies.zip")
+  if (downloadData == TRUE & !file.exists(myPathFileName)) {
     download.file(remoteDataName, myPathFileName)
     unzip(zipfile = myPathFileName) # NSM: am having problems explicitly feeding an argument to "exdir" for this function.
                                     # For now, it's using the current working directory as the default
@@ -34,14 +40,11 @@ getAcs <- function(pullYear, pullSpan, pullState, pullSt, pullCounties, pullTabl
 #----------------------------
 
   # Identify the sequence number corresponding to each table that has been specified
-    #myMeta <- Meta[Meta$Table.ID %in% pullTables, ]
-    Meta$ElemName <- paste0(Meta$Table.ID, "_", Meta$Line.Number)
+    Meta$elemName <- paste0(Meta$Table.ID, "_", Meta$Line.Number)
+  # Subset the metafile to only information pertaining to table columns
     myMeta <- Meta[!is.na(Meta$Line.Number) & Meta$Line.Number %% 1 == 0, ]; rm(Meta)
 
-    geoLabels <- read.csv(paste0(rootDir, "data/prepped-data/geofile-fields.csv"), header=T) # created by hand from documentation
-    geoFile <- read.csv(paste0(dlDir, "g", pullYear, pullSpan, tolower(pullSt), ".csv"), header=F)
-    colnames(geoFile) <- geoLabels$geoField
-    myLogRecNos <- geoFile$LOGRECNO[geoFile$COUNTY %in% pullCountyCodes & geoFile$SUMLEVEL == 50]
+    myLogRecNos <- geoFile$LOGRECNO[geoFile$COUNTY %in% pullCountyCodes & geoFile$SUMLEVEL == 50] # XXX Implicitly only allows draws of county data. Need to update this.
 
     seqFile.dict <- list(c("FILEID", "File Identification"),
                          c("FILETYPE", "File Type"),
@@ -57,36 +60,42 @@ getAcs <- function(pullYear, pullSpan, pullState, pullSt, pullCounties, pullTabl
 #-----------------------------------
 
     for (t in pullTables) {
-      # Compile meta-data related to the table
-      t.seqNum <- myMeta[myMeta$Table.ID == t, "Sequence.Number"][1] # We can take the first element, since all of the returned sequence numbers should be the same
-      seqFile.elemNames <- myMeta$ElemName[   myMeta$Sequence.Number == t.seqNum]
-      t.elemNames       <- myMeta$ElemName[   myMeta$Table.ID        == t       ]
-      tableMeta <- acs.lookup(endyear = 2011, span = pullSpan, dataset = "acs", table.name = t) # using year 2011 since the ACS package hasn't been updated to expect 2012 and throws an error. 2011 gets us the same results in terms of table information.
-      t.dataLabels <- tableMeta@results$variable.name
-      mySeqColNames <- c(seqFile.idVars, seqFile.elemNames)
-      t.dataDict    <- cbind(t.elemNames, t.dataLabels)
-        
-      # Identify the proper sequence table and pull the appropriate table columns
-      mySeq <- read.csv(paste0(dlDir, "e", pullYear, pullSpan, pullSt, sprintf("%04d", t.seqNum), "000.txt"), header=FALSE)
-      print("Working on table " %&% t)
-      colnames(mySeq) <- mySeqColNames
+      
+      # Identify the sequence file we need to open
+        t.seqNum <- myMeta[myMeta$Table.ID == t, "Sequence.Number"][1] # We can take the first element, since all of the returned sequence numbers should be the same
+      
+      # Get meta data on the sequence file
+        seqFile.elemNames <- myMeta$ElemName[myMeta$Sequence.Number == t.seqNum]
+        mySeqColNames <- c(seqFile.idVars, seqFile.elemNames)
+      
+      # Get meta data on the table we'll draw from the sequence file
+        t.elemNames <- myMeta$elemName[myMeta$Table.ID == t]
+        t.meta <- acs.lookup(endyear = 2011, span = pullSpan, dataset = "acs", table.name = t)
+          # using year 2011 since the ACS package hasn't been updated to expect 2012 and throws an error. 2011 gets us the same results in terms of table information.
+        t.dataLabels <- t.meta@results$variable.name
+        t.dataDict <- cbind(t.elemNames, t.dataLabels)
+              
+      # Open the sequence file and apply headers
+        mySeq <- read.csv(paste0(dlDir, "e", pullYear, pullSpan, pullSt, sprintf("%04d", t.seqNum), "000.txt"), header=FALSE)
+        #print("Working on table " %&% t)
+        colnames(mySeq) <- mySeqColNames
       
       # Pull the tables and geographies of interest
-      myTable <- mySeq[mySeq$LOGRECNO %in% myLogRecNos, c(seqFile.mergeVars, t.elemNames) ]
-      
+        myTable <- mySeq[mySeq$LOGRECNO %in% myLogRecNos, c(seqFile.mergeVars, t.elemNames) ]
+        
       # Compile all requested table information
-      if (t == pullTables[1]) {
-        myResults <- myTable
-        myDataDict <- t.dataDict
-      } else {
-        myResults <- merge(x=myResults, y=myTable, by=c("FILEID", "FILETYPE", "STUSAB", "LOGRECNO"))
-        myDataDict <- rbind(myDataDict, t.dataDict)
-      }
+        if (t == pullTables[1]) {
+          myResults <- myTable
+          myDataDict <- t.dataDict
+        } else {
+          myResults <- merge(x=myResults, y=myTable, by=c("FILEID", "FILETYPE", "STUSAB", "LOGRECNO"))
+          myDataDict <- rbind(myDataDict, t.dataDict)
+        }
     }
 
   rownames(myResults) <- pullCounty
 
-  return(myResults)
+  return(list(myResults, myDataDict))
 
 }
 
