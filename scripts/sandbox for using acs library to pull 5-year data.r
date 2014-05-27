55dc2ba0374bfe78182b4ae2d38406b3d597cce5
nsmader <nsmader@gmail.com>
2014-05-27 00:08:09 -0500

    Fixed loop for multiple tables

--- scripts/sandbox for using acs library to pull 5-year data.r
+++ scripts/sandbox for using acs library to pull 5-year data.r
@@ -32,15 +32,26 @@ geo.lookup(state=pullSt, county=pullPlace)
 myGeo <- geo.make(state = pullSt, county = pullCounties, tract = "*")
 acs.lookup(endyear = pullYear, span = pullSpan, dataset = "acs", table.number = c("ABCD"))
   # XXX Doesn't seem to like multiple arguments to table.number. May be because 
-acsPull <- acs.fetch(endyear = 2011, span = 5, geography = myGeo, table.number = "B17014")
 
-acsEst <- acsPull@estimate
-acsEst.df <- data.frame(acsEst)
-tract <- sapply(rownames(acsEst.df), function(x) substr(x, 14, str_locate(x, ",")[1] - 1))
-  # 14 is the first character of the tract number in the row names. Before that is "Census Tract " which is length 13.
-acsEst.df$tract <- as.numeric(tract)*100
-rownames(acsEst.df) <- NULL
-  
-
-
-
+# Pull the tables one-by-one
+  for (myT in pullTables){
+    print(paste0("Pulling data for table ", myT))
+    
+    # Pull data
+      acsPull <- acs.fetch(endyear = 2011, span = 5, geography = myGeo, table.number = myT)
+      acsEst <- acsPull@estimate
+      acsEst.df <- data.frame(acsEst)
+      
+    # Create a variable for tract
+      tract <- sapply(rownames(acsEst.df), function(x) substr(x, 14, str_locate(x, ",")[1] - 1))
+        # 14 is the first character of the tract number in the row names. Before that is "Census Tract " which is length 13.
+      acsEst.df$tract <- as.numeric(tract)*100
+      rownames(acsEst.df) <- NULL
+    
+    # Pull data together
+      if (myT == pullTables[1]) {
+        acsData <- acsEst.df
+      } else {
+        acsData <- merge(acsData, acsEst.df, by = "tract")
+      }
+  }
